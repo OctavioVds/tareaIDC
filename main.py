2700f5efb43335bf11f4b2cb0fd0f390f406eeb8
Octavio Valdes <octavio.vf@hotmail.com>
2026-03-09 21:40:20 -0600

    cambios

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 def saludar(nombre):
     return f"Hola {nombre}"
 
-def despedir(nombre):
-    return f"Adiós {nombre}"
-
-def despedir(nombre):
-    return f"Adiós {nombre}"
-
 if __name__ == '__main__':
     print(saludar("M"))
     
